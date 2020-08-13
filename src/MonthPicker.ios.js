fa5b668c48279c5864b6090b5b40b3d28fd3ad52
Christian Chown <christian.chown@santander.co.uk>
2020-08-13 17:28:11 +0100

    fix: iOS styling and operation

--- src/MonthPicker.ios.js
+++ src/MonthPicker.ios.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import {
-  View,
-  StyleSheet,
-  Dimensions,
-  Text,
-  NativeModules,
-} from 'react-native';
+import { View, StyleSheet, NativeModules } from 'react-native';
 import invariant from 'invariant';
 
 import RNMonthPickerView from './RNMonthPickerNativeComponent';
 import { ACTION_DATE_SET, ACTION_DISMISSED, useTheme } from './utils';
 
-const { width } = Dimensions.get('screen');
-
 const styles = StyleSheet.create({
-  pickerContainer: { height: 200, minWidth: 315 },
+  pickerContainer: { width: '100%', height: 200, minWidth: 315 },
+  picker: { flex: 1 },
 });
 
 class MonthPicker extends React.PureComponent {
@@ -25,9 +18,20 @@ class MonthPicker extends React.PureComponent {
   getLongFromDate = (selectedValue) => selectedValue.valueOf();
 
   onValueChange = (event) => {
-    const date = event.nativeEvent.newDate;
-    this.setState({ currentDate: date });
+    const [month, year] = event.nativeEvent.newDate.split('-');
+    this.props.onChange(
+      ACTION_DATE_SET,
+      new Date(parseInt(year, 10), parseInt(month, 10) - 1, 1),
+    );
   };
+
+  componentDidMount() {
+    const darkMode = NativeModules.RNDarkModeManager;
+    darkMode.isDarkMode((error, isDark) => {
+      const theme = this.props.enableAutoDarkMode ? isDark : !isDark;
+      this.setState({ theme: useTheme(theme) });
+    });
+  }
 
   render() {
     const {
